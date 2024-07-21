e50101b7960949848a4a39f12781754217b26d34
NagiPragalathan <nagipragalathan@gmail.com>
2024-07-21 13:23:29 +0530

    "Updated styles for HeaderSection and HeroSection components"

--- src/Home/HeaderSection.jsx
+++ src/Home/HeaderSection.jsx
@@ -34,8 +34,8 @@ const HeaderSection = () => {
                             >
                                 <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{
                                     position: 'fixed',
-                                    top: '2%',
-                                    right: '3%',
+                                    top: '26%',
+                                    right: '5%',
                                     zIndex: 100,
                                 }}>
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4 6h16M4 12h16M4 18h16"></path>
--- src/Home/HeroSection.jsx
+++ src/Home/HeroSection.jsx
@@ -12,10 +12,10 @@ const HeroSection = () => {
                     <div className="relative inline-flex mt-10 space-x-4">
                         <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
 
-                        <a href="#" title="" className="sm:px-8 sm:py-4 text-[16px] relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" role="button">
+                        <a href="#" title="" className="sm:px-8 sm:py-4 text-[15px] relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" role="button">
                             Learn More
                         </a>
-                        <a href="#" title="" className="sm:px-8 sm:py-4 text-[16px] relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-gray-900 transition-all duration-200 bg-white border border-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 hover:text-white" role="button">
+                        <a href="#" title="" className="sm:px-8 sm:py-4 text-[15px] relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-gray-900 transition-all duration-200 bg-white border border-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 hover:text-white" role="button">
                             Contact Us
                         </a>
                     </div>
